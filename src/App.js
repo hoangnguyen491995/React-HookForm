99fdc578916ff5e5fe8cf1259197de8e91694867
new name <hoangnguyen38017@gmail.com>
2022-07-01 07:15:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React from "react";
-
 import "./App.css";
-
+import { useForm } from "react-hook-form";
+import { Button } from "@mui/material";
 export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h1>React Hook Form</h1>
       <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" placeholder="Your Name" />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Your Name"
+        {...register("name", { required: true })}
+      />
       <label htmlFor="name">Password</label>
       <input
         type="password"
         name="password"
         id="password"
         placeholder="Password"
+        {...register("password", { required: true, minLength: 6 })}
       />
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" placeholder="Your Email" />
+      <input
+        {...register("email", {
+          required: true,
+          pattern:
+            /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+        })}
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Your Email"
+      />
+      {Object.keys(errors).length !== 0 && (
+        <ul className="error-container">
+          {errors.name?.type === "required" && <li>Name is required</li>}
+          {errors.password?.type === "required" && (
+            <li>password is required</li>
+          )}
+          {errors.password?.type === "minLength" && (
+            <li>password must be 6 charactes long</li>
+          )}
+          {errors.email?.type === "required" && <li> email is required</li>}
+          {errors.email?.type === "pattern" && <li> Invalid email address</li>}
+        </ul>
+      )}
 
-      <ul className="error-container">
-        <li>Name is required</li>
-      </ul>
-
-      <button type="submit">Submit</button>
+      <Button variant='contained' type="submit">Submit</Button>
     </form>
   );
 }
